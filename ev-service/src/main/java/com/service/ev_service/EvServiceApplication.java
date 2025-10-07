2558a07edebff945bf7b73f07a49934b7628215e
hienpcc <hienpcthichlamit@gmail.com>
2025-10-07 12:33:08 +0700

    start

---
+++ ev-service/src/main/java/com/service/ev_service/EvServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.ev_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvServiceApplication.class, args);
+	}
+
+}
